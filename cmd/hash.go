67ec4ff22505e718ad78c1a5b4c80c9c2b136e69
E Camden Fisher <fish@fishnix.net>
2020-06-25 14:29:43 -0400

    license line

--- cmd/hash.go
+++ cmd/hash.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 E Camden Fisher <fish@fishnix.net>
+Copyright © 2020 Yale University
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
