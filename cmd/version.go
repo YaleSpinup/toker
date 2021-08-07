15efd055d6f8a71f1b6402c99b57bf0090158e85
E Camden Fisher <fish@fishnix.net>
2021-08-07 07:26:41 -0400

    add version and move to goreleaser (#4)
    * add version and move to goreleaser

---
+++ cmd/version.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2020 Yale University
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+type CmdVersion struct {
+	AppVersion string
+	BuildTime  string
+	GitCommit  string
+	GitRef     string
+}
+
+var (
+	Version *CmdVersion
+	long    bool
+
+	versionCmd = &cobra.Command{
+		Use:     "version",
+		Aliases: []string{"vers"},
+		Short:   "Display version information",
+		RunE: func(_ *cobra.Command, args []string) error {
+			if long {
+				fmt.Printf("Toker version: %s\nBuildtime: %s\nGitCommit: %s\n", Version.AppVersion, Version.BuildTime, Version.GitCommit)
+				return nil
+			}
+
+			fmt.Printf("%s\n", Version.AppVersion)
+			return nil
+		},
+	}
+)
+
+func init() {
+	versionCmd.Flags().BoolVarP(&long, "long", "l", false, "get more verbose version information")
+	rootCmd.AddCommand(versionCmd)
+}
--- main.go
+++ main.go
@@ -18,6 +18,18 @@ package main
 
 import "github.com/YaleSpinup/toker/cmd"
 
+var (
+	version = "v0.0.0"
+	date    = "unset"
+	commit  = "unset"
+)
+
 func main() {
+	cmd.Version = &cmd.CmdVersion{
+		AppVersion: version,
+		BuildTime:  date,
+		GitCommit:  commit,
+	}
+
 	cmd.Execute()
 }
